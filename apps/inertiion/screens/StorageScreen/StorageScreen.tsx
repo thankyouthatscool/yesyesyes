cd1df4e0a8ee14548203f7bb68173178b293fb89
thankyouthatscool <thankyouthatscool@gmail.com>
2023-04-21 07:25:38 +1000

    started working on sql for storage search

--- apps/inertiion/screens/StorageScreen/StorageScreen.tsx
+++ apps/inertiion/screens/StorageScreen/StorageScreen.tsx
@@ -1,17 +1,13 @@
 import _debounce from "lodash.debounce";
 import { FC, useCallback, useEffect, useState } from "react";
-import { BackHandler, ScrollView, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { Card, Searchbar, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useAppDispatch, useAppSelector } from "@hooks";
 import { clearStorageSearchTerm, setStorageSearchTerm } from "@store";
 import { defaultAppPadding } from "@theme";
-import {
-  StorageCardComponentNavProps,
-  StorageLocationData,
-  StorageScreenProps,
-} from "@types";
+import { StorageLocationData, StorageScreenProps } from "@types";
 
 export const StorageScreen: FC<StorageScreenProps> = ({ navigation }) => {
   const dispatch = useAppDispatch();
@@ -27,6 +23,11 @@ export const StorageScreen: FC<StorageScreenProps> = ({ navigation }) => {
   const [isFocus, setIsFocus] = useState<boolean>(false);
   const [isLoadingStorageData, setIsLoadingStorageData] =
     useState<boolean>(false);
+  const [isLoadingSearchResults, setIsLoadingSearchResults] =
+    useState<boolean>(false);
+  const [storageSearchResult, setStorageSearchResult] = useState<
+    StorageLocationData[]
+  >([]);
 
   const handleLoadStorageData = useCallback(() => {
     setIsLoadingStorageData(() => true);
@@ -74,6 +75,39 @@ export const StorageScreen: FC<StorageScreenProps> = ({ navigation }) => {
     );
   }, []);
 
+  const handleStorageSearch = useCallback(
+    _debounce((storageSearchTerm: string) => {
+      console.log(`Searching for ${storageSearchTerm}`);
+
+      db.transaction(
+        (tx) => {
+          tx.executeSql(
+            `
+            SELECT *
+            FROM storage
+            INNER JOIN items
+            ON items.id = storage.itemId
+            WHERE items.code LIKE ?
+            OR items.location LIKE ?
+          `,
+            [`%${storageSearchTerm}%`, `%${storageSearchTerm}%`],
+            (_, { rows: { _array } }) => {
+              console.log(_array);
+
+              setStorageSearchResult(() => _array);
+            }
+          );
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+
+      setIsLoadingSearchResults(() => false);
+    }, 500),
+    []
+  );
+
   useEffect(() => {
     handleLoadStorageData();
   }, []);
@@ -83,6 +117,13 @@ export const StorageScreen: FC<StorageScreenProps> = ({ navigation }) => {
       handleLoadAllLocationData();
     }
   }, [distinctLocations]);
+
+  useEffect(() => {
+    // if (storageSearchTerm.length > 2) {
+    //   setIsLoadingSearchResults(() => true);
+    //   handleStorageSearch(storageSearchTerm);
+    // }
+  }, [storageSearchTerm]);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -107,7 +148,6 @@ export const StorageScreen: FC<StorageScreenProps> = ({ navigation }) => {
         }}
         value={storageSearchTerm}
       />
-
       {!isLoadingStorageData && !!distinctLocations.length ? (
         <ScrollView>
           {distinctLocations.map((loc) => (
@@ -164,8 +204,6 @@ export const StorageScreen: FC<StorageScreenProps> = ({ navigation }) => {
                         <Text>{ls.location}</Text>
                       </View>
                       {!!ls.description && <Text>{ls.description}</Text>}
-                      {/* {!!ls.cartons && <Text>Cartons: {ls.cartons}</Text>} */}
-                      {/* {!!ls.pieces && <Text>Pieces: {ls.pieces}</Text>} */}
                       {!!ls.cartons && !!ls.pieces && (
                         <View style={{ flexDirection: "row" }}>
                           <Text
@@ -197,91 +235,11 @@ export const StorageScreen: FC<StorageScreenProps> = ({ navigation }) => {
       ) : (
         <Text>Loading</Text>
       )}
+      {/* {storageSearchTerm.length > 2 ? (
+        <Text>Search results</Text>
+      ) : (
+        <Text>All Data...</Text>
+      )} */}
     </SafeAreaView>
-  );
-};
-
-const StorageCardComponent: FC<{
-  location: string;
-  nav: StorageCardComponentNavProps;
-}> = ({ location, nav }) => {
-  const { databaseInstance: db, storageSearchTerm } = useAppSelector(
-    ({ app }) => ({ ...app })
-  );
-
-  const [locationData, setLocationData] = useState<StorageLocationData[]>([]);
-
-  const handleGetStorageLocationData = useCallback(() => {
-    db.transaction(
-      (tx) => {
-        tx.executeSql(
-          ` SELECT *
-            FROM storage
-            INNER JOIN items
-            on items.id = storage.itemId
-            WHERE storage.storageLocation = ?`,
-          [location],
-          (_, { rows: { _array } }) =>
-            setLocationData(() => _array as StorageLocationData[])
-        );
-      },
-      (err) => console.log(err)
-    );
-  }, [location]);
-
-  useEffect(() => {
-    if (!!location) {
-      handleGetStorageLocationData();
-    }
-  }, [location]);
-
-  return (
-    // TODO: Redo these cards, maybe have two columns.
-    // FIXME: ^^Same as the above, just wanted to have some fun with the colors.
-    // TODO: Also a good chance that will need to convert to a flat-list for better performance at some stage.
-    <Card
-      onPress={() => {
-        nav.navigate("StorageLocationScreen", {
-          locationName: location,
-        });
-      }}
-      style={{
-        display: locationData.some((loc) =>
-          `${loc.code} ${loc.description} ${loc.color} ${loc.location}`
-            .trim()
-            .toLowerCase()
-            .includes(storageSearchTerm.trim().toLowerCase())
-        )
-          ? "flex"
-          : "none",
-        marginHorizontal: defaultAppPadding,
-        marginVertical: defaultAppPadding / 2,
-      }}
-    >
-      <Card.Title title={location} titleVariant="titleLarge" />
-      <Card.Content>
-        {locationData.map((loc, idx) => {
-          return (
-            <View
-              key={`${location} - ${loc.id} - ${loc.itemId}`}
-              style={{ marginTop: !idx ? 0 : defaultAppPadding }}
-            >
-              <Text variant="titleSmall">
-                {loc.code}
-                {!!loc.color ? <Text> - {loc.color}</Text> : ""}
-                {!!loc.size ? ` - ${loc.size}` : ""} - {loc.location}
-              </Text>
-              <Text>
-                Cartons:{" "}
-                <Text style={{ fontWeight: "700" }}>{loc.cartons}</Text>
-              </Text>
-              <Text>
-                Pieces: <Text style={{ fontWeight: "700" }}>{loc.pieces}</Text>
-              </Text>
-            </View>
-          );
-        })}
-      </Card.Content>
-    </Card>
   );
 };
